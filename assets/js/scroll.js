a11f1d6692f8be142a8700520b8883da0abdaf13
Cxt73 <USERNAME@users.noreply.github.com>
2022-02-25 18:15:01 +0900

    update

---
+++ assets/js/scroll.js
@@ -0,0 +1,11 @@
+var _window = $(window)
+scr();
+
+function scr(){
+    var scrollTop = _window.scrollTop();
+    if (scrollTop > 260){
+        $("header").css({"height" : "60px"});
+    }else{
+        $("#site-header").css({"height" : 320 - scrollTop +"px"});
+    }
+};
