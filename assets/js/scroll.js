50adc394a77034ae47ca035ac1f649c91a1e39e2
Cxt73 <USERNAME@users.noreply.github.com>
2022-02-26 00:15:53 +0900

    update

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -5,8 +5,10 @@ function scr(){
     var scrollTop = _window.scrollTop();
     if (scrollTop > 260){
         $("header").css({"height" : "60px"});
+        $("#headerBackground").css({"filter" : "blur(13px)"});
     }else{
         $("header").css({"height" : 320 - scrollTop +"px"});
+        $("#headerBackground").css({"filter" : "blur("+(scrollTop/20)+"px)"});
     }
 };
 
